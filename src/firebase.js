df38b25683d34b9711b35eac703ae3a9ebfd8003
Brian Mcloughlin <brian.mcloughlin92@gmail.com>
2024-11-22 20:58:56 -0500

    Netflix Clone Part 1
    Nav bar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDby1U4IXdcqWCJzrPLenI0PHJd2QM7Gh4",
+  authDomain: "netflix-clone-3eb65.firebaseapp.com",
+  projectId: "netflix-clone-3eb65",
+  storageBucket: "netflix-clone-3eb65.firebasestorage.app",
+  messagingSenderId: "941438597868",
+  appId: "1:941438597868:web:68beffe61323d6f231c3c9",
+};
